1392e0d0997639dd3e44b98f3a11d3918db01024
Eslam <EAli@Appliedscienceint.com>
2022-03-30 17:20:34 +0200

    first commit

---
+++ SqlDependency2/Notifications.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlDependency2
+{
+    public class Notifications
+    {
+        public string Message { get; set; }
+        public string Url { get; set; }
+    }
+}
---
+++ SqlDependency2/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using SqlDependency2;
+using System;
+using TableDependency.SqlClient;
+using TableDependency.SqlClient.Base;
+using TableDependency.SqlClient.Base.EventArgs;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        private static string _con = "Server=.;Database=ERP;Trusted_Connection=True;";
+         
+        static void Main(string[] args)
+        {
+            var mapper = new ModelToTableMapper<Notifications>();
+            mapper.AddMapping(c => c.Message, "Message");
+            mapper.AddMapping(c => c.Url, "Url");
+
+            using (var dep = new SqlTableDependency<Notifications>(_con, "Notifications", "Identity" ,mapper: mapper))
+            {
+                dep.OnChanged += Changed;
+                dep.Start();
+
+                Console.WriteLine("Press a key to exit");
+                Console.ReadKey();
+
+                dep.Stop();
+            }
+        }
+
+        public static void Changed(object sender, RecordChangedEventArgs<Notifications> e)
+        {
+            var changedEntity = e.Entity;
+
+            Console.WriteLine("DML operation: " + e.ChangeType);
+            Console.WriteLine("Message: " + changedEntity.Message);
+            Console.WriteLine("Url: " + changedEntity.Url);
+            //Console.WriteLine("Surname: " + changedEntity.Surname);
+        }
+    }
+}
+
+
+
+
+
